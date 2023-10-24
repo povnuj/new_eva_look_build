6548559e97276dcfcf40dcb48913739cdf40663a
Povnuj <Mykola.Losyk@gmail.com>
2023-10-24 16:02:18 +0300

    new files

---
+++ _nuxt/a6fa81e.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[24],{595:function(n,e,t){"use strict";t.r(e);var l={name:"Terms",layout:"default"},o=t(24),component=Object(o.a)(l,(function(){return(0,this._self._c)("h1",[this._v("wwe")])}),[],!1,null,null,null);e.default=component.exports}}]);
