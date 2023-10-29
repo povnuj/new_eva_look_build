590549b3024c8197e72909b1dc964df92866494a
Povnuj <Mykola.Losyk@gmail.com>
2023-10-29 16:54:51 +0200

    v8

--- _nuxt/static/1698590173/ua/project/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1698590173",layout:"white",error:a,serverRendered:true,routePath:"\u002Fua\u002Fproject",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
